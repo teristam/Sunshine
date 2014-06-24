59ee300cc4a25ee7f59e76e9471b929c5ea258f8
Sarah Spikes <sdspikes@cs.stanford.edu>
2014-06-24 13:34:16 -0700

    5.14 Coding Task for Today Item on Tablet

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             // (res/layout-sw600dp). If this view is present, then the activity should be
             // in two-pane mode.
             mTwoPane = true;
-
             // In two-pane mode, show the detail view in this activity by
             // adding or replacing the detail fragment using a
             // fragment transaction.
@@ -49,6 +48,10 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
                     .replace(R.id.weather_detail_container, new DetailFragment())
                     .commit();
         }
+
+        ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_forecast));
+        forecastFragment.setUseTodayLayout(!mTwoPane);
     }
 
 
